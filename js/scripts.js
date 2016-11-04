9a09de95a35058a979418e4b7fa1f378f695821c
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-04 08:20:09 -0700

    add jquery functions and price

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,42 @@
 var bigSize = ["Large", "X-Large"];
-var veterans  = ["Veterans"];
-function Pizza(pSize, sides, drinks, price) {
+var crust = ["Stuffed cheese", "Pretzel"];
+function Pizza(pSize, crust, topping, top1, top2, price) {
   this.pSize = pSize;
-  this.sides = sides;
-  this.drinks = drinks;
+  this.crust = crust;
+  this.topping = topping;
+  this.top1 = top1;
+  this.top2 =top2;
   this.price = 20;
 }
 Pizza.prototype.addToPrice = function () {
-  if(newReleases.includes(this.pSize)) {
+  if(bigSize.includes(this.pSize)) {
     this.price += 6;
   }
-  if(senior.includes(this.sides)) {
-    this.price -= 2;
-  }
-  if(drinks.includes(this.drinks)) {
-    this.price +=2;
+  if(crust.includes(this.crust)) {
+    this.price += 2;
   }
 };
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-    var pSize = $("#").val();
-    var Sides = $("#").val();
-    var drinks = $("#").val();
+    var pSize = $("#size").val();
+    var crust = $("#crust").val();
+    var top = $("#top").val();
+    var top1 = $("#top1").val();
+    var top2 = $("#top2").val();
     var price = 20;
-    var newPizza = new Pizza(pSize, sides, drinks, price);
+    var newPizza = new Pizza(pSize, crust, top, top1, top2, price);
     newPizza.addToPrice();
 
-    $("#").append(newPizza.pSize);
-    $("#").append(newPizza.sides);
-    $("#").append(newPizza.drinks);
-    $("#").append(newPizza.price);
+    $("#selectedSize").append(newPizza.pSize);
+    $("#selectedCrust").append(newPizza.crust);
+    $("#selectedTop").append(newPizza.topping);
+    $("#selectedTop1").append(newPizza.top1);
+    $("#selectedTop2").append(newPizza.top2);
+    $("#selectedPrice").append(newPizza.price);
 
     $("#inputForm").hide();
-    $("#").show();
+    $("#recipt").show();
 
   });
 });
