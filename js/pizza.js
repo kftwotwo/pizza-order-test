c94e6df6a6e084b0b614f93f84e06da9edf0d82b
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-05 16:37:17 -0700

    add js logic

--- js/pizza.js
+++ js/pizza.js
@@ -1,60 +1,60 @@
-var pizzaSizes = [
-  "small",
-  "medium",
-  "large",
-  "xlarge"
-]
+var pizzaSizes = {
+  "small": 10,
+  "medium": 12,
+  "large": 14,
+  "xlarge": 16
+}
 
-var pizzaCrusts = [
-  "hand-tossed",
-  "thin",
-  "stuffed",
-  "pretzel",
-  "deep-dish"
-]
+var pizzaCrusts = {
+  "hand-tossed": 0,
+  "thin": 0,
+  "stuffed": 2,
+  "pretzel": 2,
+  "deep-dish": 1
+}
 
-var pizzaCheese = [
-  "american",
-  "swiss",
-  "cheddar",
-  "smoked-cheddar",
-  "provolone"
-]
+var pizzaCheese = {
+  "american": 0,
+  "swiss": 0,
+  "cheddar": 0,
+  "smoked-cheddar": .5,
+  "provolone": 0
+}
 
-var pizzaSauce = [
-  "tomato-paste",
-  "garlic-basil-aiello",
-  "spicy-tomato",
-  "somked-white-cheedar-alfredo"
-]
+var pizzaSauce = {
+  "tomato-paste": 0,
+  "garlic-basil-aiello": 1,
+  "spicy-tomato": .6,
+  "somked-white-cheedar-alfredo": 1
+}
 
-var pizzaMeat = [
-  "ground-beef",
-  "Smoked-apple-wood-bacon",
-  "sausage",
-  "peperoni",
-  "candian-bacon"
-]
+var pizzaMeats = {
+  "ground-beef": .2,
+  "Smoked-apple-wood-bacon": .2,
+  "sausage": .2,
+  "peperoni": .2,
+  "candian-bacon": .2
+}
 
-var pizzaVeggie = [
-  "lettece",
-  "oinion",
-  "hummus",
-  "bell-peppers"
-]
+var pizzaVeggie = {
+  "lettece": .2,
+  "oinion": .3,
+  "hummus": .2,
+  "bell-peppers": .3
+}
 
 function pizzaOptions (options) {
   var optionList = "";
-  for (var i = 0; i < options.length; i++) {
-    optionList += "<option value='" + options[i] + "'>" + options[i].capitalizeFirstLetter().removeDashes() + "</option><br>";
+  for (var key in options) {
+    optionList += "<option value='" + key + "'>" + key.capitalizeFirstLetter().removeDashes() + "</option><br>";
   }
   return optionList;
 }
 
 function pizzaToppings (toppings) {
   var toppingList = "";
-  for (var i = 0; i < toppings.length; i++) {
-    toppingList += "<label><input type='checkbox' value='" + toppings[i] + "'>" + toppings[i].capitalizeFirstLetter().removeDashes() + "</label><br>"
+  for (var key in toppings) {
+    toppingList += "<label><input type='checkbox' name='options[]' value='" + key + "'>" + key.capitalizeFirstLetter().removeDashes() + "</label><br>"
   }
   return toppingList;
 }
@@ -66,12 +66,59 @@ String.prototype.removeDashes = function () {
   return this.replace(/-|\s/g, " ");
 }
 
+class Pizza {
+  constructor(pSize, crust, sauce, cheese, toppings) {
+    this.pSize = pSize;
+    this.crust = crust;
+    this.sauce = sauce;
+    this.cheese = cheese;
+    this.toppings = toppings;
+  }
 
+  toppingsTotal () {
+    var toppingsPrice = 0;
+    for (var i = 0; i < this.toppings.length; i++) {
+      if (pizzaMeats[this.toppings[i]] != undefined) {
+        toppingsPrice += pizzaMeats[this.toppings[i]];
+      } else if (pizzaVeggie[this.toppings[i]] != undefined) {
+        toppingsPrice += pizzaVeggie[this.toppings[i]];
+      }
+      pizzaToppings[this.toppings[i]]
+    }
+    return toppingsPrice;
+  }
+}
+
+Pizza.prototype.price = function () {
+  return pizzaSizes[this.pSize] + pizzaCrusts[this.crust] + pizzaSauce[this.sauce]+ pizzaCheese[this.cheese] + this.toppingsTotal();
+}
+var order = [];
 $(document).ready(function() {
   $("#size").append(pizzaOptions(pizzaSizes));
   $("#crust").append(pizzaOptions(pizzaCrusts));
   $("#cheese").append(pizzaOptions(pizzaCheese));
   $("#sauce").append(pizzaOptions(pizzaSauce));
-  $("#meat").append(pizzaToppings(pizzaMeat))
+  $("#meat").append(pizzaToppings(pizzaMeats))
   $("#veggie").append(pizzaToppings(pizzaVeggie))
+
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+    var pSize = $("#size").val();
+    var crust = $("#crust").val();
+    var sauce = $("#sauce").val();
+    var cheese = $("#cheese").val();
+    var toppings = [];
+    $("input[name='options[]']:checked").each(function () {
+          toppings.push($(this).val());
+      });
+    var newPizza = new Pizza (pSize, crust, sauce, cheese, toppings);
+    order.push(new Pizza)
+    $("#selectedSize").append(newPizza.pSize);
+    $("#selectedCrust").append(newPizza.crust);
+    $("#selectedSauce").append(newPizza.sauce);
+    $("#selectedCheese").append(newPizza.cheese);
+    $("#selectedToppings").append(newPizza.toppings.join(", "));
+    $("#selectedPrice").append(newPizza.price());
+    $("#recipt").show();
+  });
 })
--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,51 @@
-var bigSize = ["Large-18", "X-Large-20"];
-var normal = ["Small", "Medium"]
-var crust = ["Stuffed cheese", "Pretzel"];
-var sauce = ["Somked White Cheedar Alfredo","Garlic Basil Aiello"]
-function Pizza(pSize, crust, sauce, cheese, meat, veggie, price) {
-  this.pSize = pSize;
-  this.crust = crust;
-  this.sauce = sauce;
-  this.cheese = cheese;
-  this.meat = meat;
-  this.veggie =veggie;
-  this.price = 0;
-}
-Pizza.prototype.addToPrice = function () {
-  if(bigSize.includes(this.pSize)) {
-    this.price += 25.39;
-  }
-  if(normal.includes(this.pSize)) {
-    this.price += 20.54;
-  }
-  if(sauce.includes(this.sauce)) {
-    this.price += 2.55;
-  }
-  if(crust.includes(this.crust)) {
-    this.price += 2.01;
-  }
-};
-$(document).ready(function() {
-  $("#inputForm").submit(function(event) {
-    event.preventDefault();
-    var pSize = $("#size").val();
-    var crust = $("#crust").val();
-    var sauce = $("#sauce").val();
-    var cheese = $("#cheese").val();
-    var meat = $("#meat").val();
-    var veggie = $("#veggie").val();
-    price = 0;
-    var newPizza = new Pizza(pSize, crust, sauce, cheese, meat, veggie, price);
-    newPizza.addToPrice();
-
-    $("#selectedSize").append(newPizza.pSize);
-    $("#selectedCrust").append(newPizza.crust);
-    $("#selectedSauce").append(newPizza.sauce);
-    $("#selectedCheese").append(newPizza.cheese);
-    $("#selectedMeat").append(newPizza.meat);
-    $("#selectedveggie").append(newPizza.veggie);
-    $("#selectedPrice").append(newPizza.price.toFixed(2));
-    $("#inputForm").hide();
-    $("#recipt").show();
-  });
-});
+// var bigSize = ["Large-18", "X-Large-20"];
+// var normal = ["Small", "Medium"]
+// var crust = ["Stuffed cheese", "Pretzel"];
+// var sauce = ["Somked White Cheedar Alfredo","Garlic Basil Aiello"]
+// function Pizza(pSize, crust, sauce, cheese, meat, veggie, price) {
+//   this.pSize = pSize;
+//   this.crust = crust;
+//   this.sauce = sauce;
+//   this.cheese = cheese;
+//   this.meat = meat;
+//   this.veggie =veggie;
+//   this.price = 0;
+// }
+// Pizza.prototype.addToPrice = function () {
+//   if(bigSize.includes(this.pSize)) {
+//     this.price += 25.39;
+//   }
+//   if(normal.includes(this.pSize)) {
+//     this.price += 20.54;
+//   }
+//   if(sauce.includes(this.sauce)) {
+//     this.price += 2.55;
+//   }
+//   if(crust.includes(this.crust)) {
+//     this.price += 2.01;
+//   }
+// };
+// $(document).ready(function() {
+//   $("#inputForm").submit(function(event) {
+//     event.preventDefault();
+//     var pSize = $("#size").val();
+//     var crust = $("#crust").val();
+//     var sauce = $("#sauce").val();
+//     var cheese = $("#cheese").val();
+//     var meat = $("#meat").val();
+//     var veggie = $("#veggie").val();
+//     var price = 0;
+//     var newPizza = new Pizza(pSize, crust, sauce, cheese, meat, veggie, price);
+//     newPizza.addToPrice();
+//
+//     $("#selectedSize").append(newPizza.pSize);
+//     $("#selectedCrust").append(newPizza.crust);
+//     $("#selectedSauce").append(newPizza.sauce);
+//     $("#selectedCheese").append(newPizza.cheese);
+//     $("#selectedMeat").append(newPizza.meat);
+//     $("#selectedveggie").append(newPizza.veggie);
+//     $("#selectedPrice").append(newPizza.price.toFixed(2));
+//     $("#inputForm").hide();
+//     $("#recipt").show();
+//   });
+// });
